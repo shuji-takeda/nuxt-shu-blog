46a23d2e33a53b216d36448df32b2c9193ed612a
shuji-takeda <12.hack.17.takeshu@gmail.com>
2025-04-06 08:34:09 +0900

    eslint,tsconfig error resolve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
+export default withNuxt({
   // Your custom configs here
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": "error",
-    "vue/multi-word-component-names": "off"
-  }
-)
+    "vue/multi-word-component-names": "off",
+  },
+});
