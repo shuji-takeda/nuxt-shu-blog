6ab33ac026b950e8e610743dbfce43093aae86d2
shuji-takeda <12.hack.17.takeshu@gmail.com>
2025-04-06 18:58:17 +0900

    add-vuetify-plugin

--- components/common/AppFooter.vue
+++ components/common/AppFooter.vue
@@ -1,57 +1,43 @@
 <template>
-  <footer class="bg-[#fafafa] text-gray-700 py-4 text-center mt-auto">
-    <div class="container mx-auto flex flex-col items-center space-y-2">
-      <div class="flex space-x-4">
-        <a
+  <v-footer class="text-grey-darken-2 mt-auto" color="#fafafa" padless>
+    <v-container class="text-center py-2">
+      <v-row justify="center" class="pa-0 ma-0" no-gutters>
+        <v-btn
+          icon
           href="https://github.com/shuji-takeda"
           target="_blank"
-          class="text-gray-500 hover:text-gray-800"
+          variant="text"
+          color="grey"
         >
-          <svg
-            class="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path
-              d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.66-.22.66-.48 0-.24-.01-1.04-.01-1.89-2.49.46-3.02-.61-3.21-1.17-.11-.28-.59-1.17-1.01-1.41-.34-.18-.83-.63-.01-.64.77-.01 1.32.71 1.5 1.01.87 1.47 2.27 1.05 2.83.8.09-.63.34-1.05.62-1.29-2.22-.25-4.56-1.11-4.56-4.94 0-1.09.39-1.99 1.02-2.69-.1-.25-.44-1.28.1-2.66 0 0 .83-.27 2.72 1.02a9.42 9.42 0 0 1 4.94 0c1.9-1.29 2.72-1.02 2.72-1.02.54 1.38.2 2.41.1 2.66.63.7 1.02 1.6 1.02 2.69 0 3.84-2.35 4.69-4.59 4.94.35.3.66.9.66 1.81 0 1.31-.01 2.36-.01 2.69 0 .27.16.58.67.48A10.01 10.01 0 0 0 22 12c0-5.52-4.48-10-10-10z"
-            ></path>
-          </svg>
-        </a>
-        <a
+          <v-icon size="24">mdi-github</v-icon>
+        </v-btn>
+
+        <v-btn
+          icon
           href="https://www.linkedin.com/in/shuji-takeda-601041237/"
           target="_blank"
-          class="text-gray-500 hover:text-gray-800"
+          variant="text"
+          color="grey"
         >
-          <svg
-            class="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path
-              d="M20.45 2H3.55C2.7 2 2 2.7 2 3.55v16.9C2 21.3 2.7 22 3.55 22h16.9c.85 0 1.55-.7 1.55-1.55V3.55C22 2.7 21.3 2 20.45 2zM8.41 18H5.34V9.48h3.07V18zM6.88 8.34c-.98 0-1.78-.8-1.78-1.78s.8-1.78 1.78-1.78 1.78.8 1.78 1.78-.8 1.78-1.78 1.78zm12.12 9.66h-3.07v-4.63c0-1.1-.02-2.52-1.53-2.52-1.53 0-1.77 1.2-1.77 2.44v4.71h-3.07V9.48h2.94v1.16h.04c.41-.78 1.42-1.6 2.92-1.6 3.12 0 3.7 2.06 3.7 4.74V18z"
-            />
-          </svg>
-        </a>
-        <a
+          <v-icon size="24">mdi-linkedin</v-icon>
+        </v-btn>
+
+        <v-btn
+          icon
           href="https://x.com/takeshshuhu"
           target="_blank"
-          class="text-gray-500 hover:text-gray-800"
+          variant="text"
+          color="grey"
         >
-          <svg
-            class="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path
-              d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
-            ></path>
-          </svg>
-        </a>
-      </div>
-      <p>&copy; 2025 Shu Blog. All rights reserved.</p>
-    </div>
-  </footer>
+          <v-icon size="24">mdi-twitter</v-icon>
+        </v-btn>
+      </v-row>
+
+      <v-row justify="center" class="pa-0 ma-0" no-gutters>
+        <v-col cols="12" class="text-center text-caption mt-1">
+          &copy; 2025 Shu Blog. All rights reserved.
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
